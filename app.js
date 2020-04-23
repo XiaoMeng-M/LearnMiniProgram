5cdca8e8181535943170cb0214dd646554dda455
mengyaqi <2686587087@qq.com>
2020-04-23 21:04:00 +0800

    04-微信系统弹窗以及token'

--- app.js
+++ app.js
@@ -10,28 +10,45 @@ App({
 
     const token = wx.getStorageSync('token')
     if (token && token.length !== 0) {
-      
-    }else {
-      wx.login({
-        success: (res) => {
-          const code = res.code
-          // 把code发送到服务器
-          wx.request({
-            url: 'http://123.207.32.32:3000/login',
-            method : 'post',
-            data: {
-              code
-            },
-            success: (res)=> {
-              const token = res.data.token
-              this.globalData.token = token
-              wx.setStorageSync('token', token)
-              console.log(this.globalData.token);
-            }
-          })
+      wx.request({
+        url: 'http://123.207.32.32:3000/auth',
+        method: "post",
+        header: {
+          token
         },
+        success: res=>{
+          if (!res.data.errCode) {
+           console.log("token有效");
+            this.globalData.token = token
+          }else {
+            this.login()
+          }
+        }
       })
+    }else {
+      this.login()
     }
 
+  },
+  login(){
+    wx.login({
+      success: (res) => {
+        const code = res.code
+        // 把code发送到服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method : 'post',
+          data: {
+            code
+          },
+          success: (res)=> {
+            const token = res.data.token
+            this.globalData.token = token
+            wx.setStorageSync('token', token)
+            console.log(this.globalData.token);
+          }
+        })
+      },
+    })
   }
 })
